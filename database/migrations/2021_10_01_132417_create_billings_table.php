e95f9571a43f77d4a3d856032504036293319bfa
Arman Arif <arman3472rmn@gmail.com>
2021-10-02 03:35:02 +0600

    Migration files for billing table

---
+++ database/migrations/2021_10_01_132417_create_billings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('user');
+            $table->string('month');
+            $table->string('amout');
+            $table->date('due_date');
+            $table->date('pay_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}
