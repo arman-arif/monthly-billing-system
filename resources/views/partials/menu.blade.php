b3f2408b630b8feee41652663c8bb6f3ee0ce3a2
Arman Arif <arman3472rmn@gmail.com>
2021-10-02 03:35:34 +0600

    Added nav and menu

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,11 @@
+<div class="mt-4 card">
+    <div class="card-body">
+        <div class="nav flex-column nav-pills">
+            <a href="#" class="nav-link active">Overview</a>
+            <a href="#" class="nav-link">Reports</a>
+            <a href="#" class="nav-link">Customers</a>
+            <a href="#" class="nav-link">Billing</a>
+            <a href="#" class="nav-link">Packages</a>
+        </div>
+    </div>
+</div>
