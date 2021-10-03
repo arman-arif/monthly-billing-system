2e4667e8b94d1bf16164346a902bfbcf2d328e11
Arman Arif <arman3472rmn@gmail.com>
2021-10-03 09:37:49 +0600

    Added package list

--- resources/views/package/package-list.blade.php
+++ resources/views/package/package-list.blade.php
@@ -1,11 +1,40 @@
-@extends('layouts.structure',['titleHeading'=>'Dashboard'])
+@extends('layouts.structure',['titleHeading'=>$title])
+
+@push('action-btn')
+    <a class="btn btn-sm btn-primary" href="{{ route('add-package') }}">Add Package</a>
+@endpush
 
 @section('content')
-    @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
-        </div>
-    @endif
-
-    {{ __('You are logged in!') }}
+    <table class="table table-sm">
+        <thead>
+        <tr>
+            {{--<th>S/N</th>--}}
+            <th>Package</th>
+            <th>Code</th>
+            <th>Speed</th>
+            <th>Duration</th>
+            <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse ($packages as $package)
+            <tr>
+                {{--<td>{{ 1 }}</td>--}}
+                <td>{{ $package->title }}</td>
+                <td>{{ Str::upper($package->code) }}</td>
+                <td>{{ $package->speed }} Mbps</td>
+                <td>{{ $package->duration }} Days</td>
+                <td>{{ $package->price }} TK</td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="5" class="text-center">
+                    <div class="pt-4">
+                        No package added yet
+                    </div>
+                </td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
 @endsection
