9fc5141a4c47a1f503d455f8e3e9ba043409b03b
arman-arif <arman3472rmn@gmail.com>
2021-10-03 13:51:46 +0600

    Added customer route links

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,11 +1,11 @@
 <div class="mt-4 card">
     <div class="card-body">
         <div class="nav flex-column nav-pills">
-            <a href="#" class="nav-link active">Overview</a>
+            <a href="#" class="nav-link">Overview</a>
             <a href="#" class="nav-link">Reports</a>
-            <a href="#" class="nav-link">Customers</a>
+            <a href="{{ route('customers') }}" class="nav-link {{ Request::url() == route('customers') ? "active" : "" }}">Customers</a>
             <a href="#" class="nav-link">Billing</a>
-            <a href="{{ route('package') }}" class="nav-link">Packages</a>
+            <a href="{{ route('package') }}" class="nav-link {{ Request::url() == route('package') ? "active" : "" }}">Packages</a>
         </div>
     </div>
 </div>
