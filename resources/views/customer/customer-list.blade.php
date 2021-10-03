6ba09d0d335272300a968255c9a2fceef1a0223d
arman-arif <arman3472rmn@gmail.com>
2021-10-03 13:49:04 +0600

    Added customer list view

---
+++ resources/views/customer/customer-list.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.structure',['titleHeading'=>$title])
+
+@push('action-btn')
+    <a class="btn btn-sm btn-primary" href="{{ route('add-package') }}">Add Customer</a>
+@endpush
+
+@push('stylesheet')
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+@endpush
+
+@section('content')
+    <table class="table table-sm">
+        <thead>
+        <tr>
+            <th>S/N</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Package</th>
+            <th>Connected</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+@endsection
