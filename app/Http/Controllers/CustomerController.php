41568aab192c2541cee3409bb82bf656458fe22b
arman-arif <arman3472rmn@gmail.com>
2021-10-04 20:04:39 +0600

    Added action for customer view

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Package;
 
 class CustomerController extends Controller
 {
     public function getCustomers(){
         return view('customer.customer-list',['title'=>'Customers']);
+    }
+
+    public function getAddCustomer()
+    {
+        $packages = Package::all();
+        return view('customer.customer-add',['title'=>'Add Customer'], compact('packages'));
+    }
+
+    public function postAddCustomer()
+    {
+        
+        return redirect()->route('add-customer');
     }
 }
