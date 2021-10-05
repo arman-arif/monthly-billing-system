b6323f55943a2dd46aa75f14cb79f163c41d5804
Arman Arif <arman3472rmn@gmail.com>
2021-10-05 16:51:32 +0600

    Changed menu link orientation

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -3,8 +3,8 @@
         <div class="nav flex-column nav-pills">
             <a href="#" class="nav-link">Overview</a>
             <a href="#" class="nav-link">Reports</a>
-            <a href="{{ route('customers') }}" class="nav-link {{ Request::url() == route('customers') ? "active" : "" }}">Customers</a>
             <a href="#" class="nav-link">Billing</a>
+            <a href="{{ route('customers') }}" class="nav-link {{ Request::url() == route('customers') ? "active" : "" }}">Customers</a>
             <a href="{{ route('package') }}" class="nav-link {{ Request::url() == route('package') ? "active" : "" }}">Packages</a>
         </div>
     </div>
